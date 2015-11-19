12da33a21489fb1cb9cd8bb5d74503e76d13c367
peter <peter@Farm.att.net>
2015-11-18 17:32:19 -0800

    rue de teh

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello world! This IS in Git.");
+		System.out.println("Hello world! This IS in Git. Now cloned on to laptop.");
 	}
 
 }
