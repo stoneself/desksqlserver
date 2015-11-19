3ded33f84adbe873c383dd0ce35940e550c6c566
peter <peter@Farm.att.net>
2015-11-18 17:35:01 -0800

    se

---
+++ toDoList/src/toDoList/ToDoList.java
@@ -0,0 +1,22 @@
+package toDoList;
+
+public class ToDoList {
+
+}
+
+
+// TODO make list that
+// reorders well
+// preserves sort order
+// that can manage multiple sort order
+// has item/title
+// has note
+// has time stamp
+// has unique id
+// has priority - no, priority is a sort order
+// has urgency
+// has importance
+// can have sub item
+// has completion status
+// has expiration?
+// has recurrence
